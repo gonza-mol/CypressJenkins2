4ce1cd246b1ff7c34c4f73f78283b6ae2695d474
gonzalo <gonzalo.molina@darwoft.com>
2023-12-14 12:42:44 -0300

    repo actualizado

---
+++ cypress/e2e/prueba.cy.js
@@ -0,0 +1,14 @@
+describe("Tests de Prueba para Jenkins", () => {
+  it("Test que PASA", () => {
+    cy.visit("https://example.cypress.io");
+  });
+
+  it("Test que FALLA", () => {
+    cy.visit("https://example.cypress.io");
+    cy.get("h1").should("have.text", "Hola Mundo!");
+  });
+
+  it.skip("Test que NO SE EJECUTA", () => {
+    cy.visit("https://example.cypress.io");
+  });
+});
